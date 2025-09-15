be7501bd2d24b11329a2d5519bc85f21fa382005
Aditya Narayan <aditya.narayan@juspay.in>
2025-09-15 20:18:48 +0530

    added division function

--- rust_calculator/src/main.rs
+++ rust_calculator/src/main.rs
@@ -1,13 +1,30 @@
-use rust_calculator::add;
+use rust_calculator::{add, multiply, divide};
 
 fn main() {
-    println!("Simple Calculator (Only Addition)");
+    println!("Simple Calculator");
 
     let list1 = [1, 2, 3, 4, 5];
     let list2 = [10, 20, 30, 40, 50];
 
     for i in 0..list1.len() {
-        let result = add(list1[i], list2[i]);
-        println!("{} + {} = {}", list1[i], list2[i], result);
+        let a = list1[i];
+        let b = list2[i];
+
+        // Addition
+        println("Addition :";)
+        let sum = add(a, b);
+        println!("{} + {} = {}", a, b, sum);
+
+        // Multiplication
+        println("Multiplication :";)
+        let product = multiply(a, b);
+        println!("{} * {} = {}", a, b, product);
+
+        // Division
+        println("Divison :";)
+        match divide(a, b) {
+            Some(quotient) => println!("{} / {} = {}", a, b, quotient),
+            None => println!("{} / {} = Division by zero is not allowed", a, b),
+        }
     }
 }
